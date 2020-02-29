f314f98b50c14b87650b067a19bf53463d253a3f
ErosenkoD <oknesore@ukr.net>
2020-02-29 09:59:52 +0200

    corect task 2 in less 8

--- lesson_8/task_2.py
+++ lesson_8/task_2.py
@@ -6,7 +6,14 @@ False иначе.
 
 
 def is_year_leap(year_now):
-    return not year_now % 4
+    if not year_now % 400:
+        return True
+    if not year_now % 4:
+        if not year_now % 100:
+            return False
+        else:
+            return True
+    return False
 
 
 print(is_year_leap(int(input('Укажите год: '))))
